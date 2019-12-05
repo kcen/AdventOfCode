138aadddf29663c3105bed24d921e326001506c5
kcen <kcen@users.noreply.github.com>
2019-12-05 14:34:02 -0800

    make that ruby answer at least readable

--- 2019/4/pt2.rb
+++ 2019/4/pt2.rb
@@ -1,2 +1,8 @@
-s,f=ARGF.read().strip.split('-')
-p (s...f).each.select{|s|s==s.chars.sort.join}.select{|s|s.chars.uniq.map{|c|s.count(c)}.any?{|c|c==2}}.length
+s,f = ARGF.read().strip.split('-') #Args
+p (s...f).each.
+    select{|s|s==s.chars.sort.join}. #Sorted integers?
+    select{|s|
+        s.chars.uniq
+            map{|c|s.count(c)}
+            any?{|c|c==2}  #Doubles?
+    }.length
