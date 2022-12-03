ca5b15b9b91c3e35143b95332b3d6caf2d67ae66
kcen <kcen@users.noreply.github.com>
2022-12-02 20:02:24 -0800

    less case statements

--- 2022/2/2_2.rb
+++ 2022/2/2_2.rb
@@ -1,14 +1,17 @@
+# {
+#     "A" => 1, #Rock
+#     "B" => 2, #Paper
+#     "C" => 3, #Scissors
+# }
+
 def points(them, me)
     case me
     when "X" # Lose
-        0 + case them
-        when "A" # Elf rock
-            3 # Scissors
-        when "B" # Elf paper
-            1 # Rock
-        else # Elf scissors
-            2 # Paper
-        end
+        0 + {
+            "A" => 3, #Scissors
+            "B" => 1, #Rock
+            "C" => 2, #Papper
+        }[them]
     when "Y" # Draw
         3 + {
             "A" => 1, #Rock
@@ -16,14 +19,11 @@ def points(them, me)
             "C" => 3, #Scissors
         }[them]
     when "Z" # Win
-        6 + case them
-        when "A" # Elf rock
-            2 # Paper
-        when "B" # Elf paper
-            3 # Scissors
-        else # Elf scissors
-            1 # Rock
-        end
+        6 + {
+            "A" => 2, #Paper
+            "B" => 3, #Scissors
+            "C" => 1, #Rock
+        }[them]
     end
 end
 
